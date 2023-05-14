3bab2e891672e160aa6bd0acc30ebefb2b5e8321
CesarAvalosCas <133596293+CesarAvalosCas@users.noreply.github.com>
2023-05-14 11:48:30 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Yo tambien te quiero muchote mi amorcito <3')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function () {
+    alert('Porque le picaste que no?(carita enojada)')
+})
